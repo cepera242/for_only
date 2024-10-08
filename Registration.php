1ac4e4da3d26db363c160a43095dd2f9b2e550cc
root <root@WIN-F4NSTCRMEDC>
2024-10-08 21:17:38 +0700

    add Registration.php

--- Registration.php
+++ Registration.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "работает";
+?>
+
+<form method="POST">
+    <a href="Index.php"><--Назад</a>
+    Имя: <input type="text" name="name" required><br>
+    Телефон: <input type="text" name="phone" required><br>
+    Почта: <input type="email" name="email" required><br>
+    Пароль: <input type="password" name="password" required><br>
+    Повторите пароль: <input type="password" name="passwordCheck" required><br>
+    <input type="submit" value="Зарегистрироваться">
+</form>
