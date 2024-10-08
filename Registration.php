b8cc355b76c5e029542eadef7a2ac85cf67cc112
root <root@WIN-F4NSTCRMEDC>
2024-10-09 01:24:33 +0700

    add Registration.php

--- Registration.php
+++ Registration.php
@@ -16,6 +16,7 @@ checkFile(DATABASE_PASSWORD);
 if ($_SERVER["REQUEST_METHOD"] === "POST") 
 {
     $name = $_POST["name"];
+    $login = $_POST["login"];
     $phone = $_POST["phone"];
     $email = $_POST["email"];
     $password = $_POST["password"];
@@ -23,8 +24,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST")
     $user = file(DATABASE_USER, FILE_IGNORE_NEW_LINES);
     $storedPasswords = file(DATABASE_PASSWORD, FILE_IGNORE_NEW_LINES);
     foreach ($user as $users) {
-        list($dbname, $dbPhone, $dbEmail) = explode(", ", $users);
-        if ($dbname === $name || $dbPhone === $phone || $dbEmail === $email) {
+        list($dblogin, $dbPhone, $dbEmail) = explode(", ", $users);
+        if ($dblogin === $login || $dbPhone === $phone || $dbEmail === $email) {
             echo "Пользователь уже зарегистрирован";
             return;
         }
@@ -33,16 +34,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST")
         echo "Пароли не совпадают";
         return;
     }
+    $time = date("Y-m-d H:i:s");
     $id = count($user) + 1;
-    file_put_contents(DATABASE_USER, "{$id}, {$name}, {$phone}, {$email}\n", FILE_APPEND);
-    file_put_contents(DATABASE_PASSWORD, "{$id}, {$password}\n", FILE_APPEND);
-    echo "Вы зарегистрированы";
+    file_put_contents(DATABASE_USER, "{$time}, {$id}, {$name}, {$login}, {$phone}, {$email}\n", FILE_APPEND);
+    file_put_contents(DATABASE_PASSWORD, "{$time}, {$id}, {$password}\n", FILE_APPEND);
+    echo "Вы успешно зарегистрированы";
 }
 ?>
 
 <form method="POST">
     <a href="Index.php"><--Назад</a><br>
     Имя: <input type="text" name="name" required><br>
+    Логин: <input type="text" name="login" required><br>
     Телефон: <input type="text" name="phone" required><br>
     Почта: <input type="email" name="email" required><br>
     Пароль: <input type="password" name="password" required><br>
